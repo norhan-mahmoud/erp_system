323f1cd210423feb27bc0879f7ef21246e3c35ef
nourhan mahmoud ali <82783712+norhan-mahmoud@users.noreply.github.com>
2026-06-12 21:44:38 +0300

    ;

--- app/Filament/Resources/Batches/BatchResource.php
+++ app/Filament/Resources/Batches/BatchResource.php
@@ -20,7 +20,7 @@ class BatchResource extends Resource
 {
     protected static ?string $model = Batch::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::PuzzlePiece;
 
     protected static ?string $recordTitleAttribute = 'code';
 
