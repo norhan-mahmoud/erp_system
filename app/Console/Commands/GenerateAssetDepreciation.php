b0bfb07515ff359174857f9eac6cdc47b2340dc1
nourhan mahmoud ali <82783712+norhan-mahmoud@users.noreply.github.com>
2026-06-14 20:53:45 +0300

    -

---
+++ app/Console/Commands/GenerateAssetDepreciation.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+
+#[Signature('app:generate-asset-depreciation')]
+#[Description('Command description')]
+class GenerateAssetDepreciation extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
